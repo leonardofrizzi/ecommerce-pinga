b3a80b1360c883ea115143744ab09198013f27f5
Leonardo Frizzi Bueno <leo_frizzi@hotmail.com>
2025-05-27 16:52:44 -0300

    fix: Set fixed domain for Stripe checkout URLs

--- client/src/app/checkout/page.tsx
+++ client/src/app/checkout/page.tsx
@@ -9,7 +9,6 @@ const stripePromise = loadStripe(
   "pk_live_51R6aKTGdQrTu9hK3LggAl4OGzSvzEhtFneb4PC3uJNCBCeD9TxNUBTWxlnxeHAwOGXCO9wyxcYT7jatAKIEi7aI"
 );
 
-
 interface CustomerForm {
   fullName: string;
   email: string;
